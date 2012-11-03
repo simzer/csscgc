ff4add5cbdaebcc4cdc080bf810ce8b66e9d8a00
Simon Laszlo <laszlo.simon@gmail.com>
2012-11-03 15:57:04 +0100

    GameArt for gems added

---
+++ gems/xoox.C
@@ -0,0 +1,25 @@
+/* C source file created by SevenuP v1.20                                */
+/* SevenuP (C) Copyright 2002-2006 by Jaime Tejedor Gomez, aka Metalbrain*/
+
+/*
+GRAPHIC DATA:
+Pixel Size:      ( 24,  24)
+Char Size:       (  3,   3)
+Sort Priorities: X char, Char line, Y char
+Data Outputted:  Gfx+Attr
+Interleave:      Sprite
+Mask:            No
+*/
+
+unsigned char xoox[81] = {
+170,170,170,  0, 16,  1,176,200,
+240, 57,209,249,159,139,156, 15,
+ 19, 13,143, 11, 12, 31,147,157,
+185,201,248, 48,208,241,128,  8,
+  0, 85, 85, 85,170,170,170,  0,
+ 16,  1,143, 11, 12, 31,147,157,
+185,201,248, 48,208,241,176,200,
+240, 57,209,249,159,139,156, 15,
+ 19, 13,128,  8,  0, 85, 85, 85,
+ 68, 68, 68, 68, 68, 68, 68, 68,
+ 68};
